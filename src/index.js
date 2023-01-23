62ae636bca1517484046d6af619d0471ae401401
Constantine Cherkashyn <cons1@protonmail.ch>
2023-01-23 13:41:16 -0500

    Working setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ document.addEventListener("DOMContentLoaded", (e) => {
     // fetch data downloaded from https://fruityvice.com/api/fruit/all
     fetch("http://localhost:3000/fruits")
     .then((resp) => resp.json())
-    .then((data) => console.log(data))
+    .then((data) => displayFruitNav(data))
 });
 
+function displayFruitNav(fruits)
+{
+    const fruitNav = document.querySelector("#navigation");
 
+    fruits.forEach(fruit => {
+        const fruitItem = document.createElement("span");
+        fruitItem.textContent = fruit.name;
+
+        fruitNav.append(fruitItem);
+    });
+}
