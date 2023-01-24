10acebd08d385436f21edc5c6f2db0f982921eb8
Constantine Cherkashyn <cons1@protonmail.ch>
2023-01-24 15:05:05 -0500

    Sort function updated

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
     fetch("http://localhost:3000/fruits")
     .then((resp) => resp.json())
     .then((data) => {
-        displayFruitNav(data);
         displayFruitDetails(data[0]);
-        searchBox(data);
+        sortForm(data);
+        sortObject(data);
     });
 });
 
 function displayFruitNav(fruits, filter)
 {
     const fruitNav = document.querySelector("#navigation");
+    fruitNav.textContent = "";
 
     fruits.forEach(fruit => {
         if (fruit.name.toLowerCase().match(filter) || filter === undefined)
@@ -50,31 +51,100 @@ function displayFruitDetails(fruit)
 
 function searchBox(fruits)
 {
-    const fruitNav = document.querySelector("#navigation");
-
     const searchBox = document.querySelector("#searchbox");
     searchBox.textContent = "Find: ";
 
     const searchInput = document.createElement("input");
     searchBox.append(searchInput);
 
+    if (searchInput.value === "")
+    {
+        displayFruitNav(fruits);
+    }
+
     searchInput.addEventListener("input", (x) => {
-        fruitNav.textContent = "";
         displayFruitNav(fruits, x.target.value.toLowerCase());
     });
+}
 
+function sortForm(fruits)
+{
     const filterFormBox = document.querySelector("#searchbox.hidden");
-
     const filterForm = document.createElement("form");
+    const br0 = document.createElement("br");
+    const br1 = document.createElement("br");
+    const br2 = document.createElement("br");
+    const br3 = document.createElement("br");
+    const br4 = document.createElement("br");
 
     const optName = document.createElement("input");
     optName.setAttribute("type", "radio");
-    optName.setAttribute("id", "byName");
-
+    optName.setAttribute("name", "sortOrder");
+    optName.setAttribute("value", "byName");
+    optName.setAttribute("checked", "true");
     const txtName = document.createTextNode("Default sort");
 
-    filterForm.append(optName, txtName);
+    const optCarbs = document.createElement("input");
+    optCarbs.setAttribute("type", "radio");
+    optCarbs.setAttribute("name", "sortOrder");
+    optCarbs.setAttribute("value", "byCarbs");
+    const txtCarbs = document.createTextNode("Carbohydrate content");
+
+    const optProtein = document.createElement("input");
+    optProtein.setAttribute("type", "radio");
+    optProtein.setAttribute("name", "sortOrder");
+    optProtein.setAttribute("value", "byProtein");
+    const txtProtein = document.createTextNode("Protein content");
+
+    const optFat = document.createElement("input");
+    optFat.setAttribute("type", "radio");
+    optFat.setAttribute("name", "sortOrder");
+    optFat.setAttribute("value", "byFat");
+    const txtFat = document.createTextNode("Fat content");
+
+    const optCalories = document.createElement("input");
+    optCalories.setAttribute("type", "radio");
+    optCalories.setAttribute("name", "sortOrder");
+    optCalories.setAttribute("value", "byCalories");
+    const txtCalories = document.createTextNode("Calorie content");
+
+    const optSugar = document.createElement("input");
+    optSugar.setAttribute("type", "radio");
+    optSugar.setAttribute("name", "sortOrder");
+    optSugar.setAttribute("value", "bySugar");
+    const txtSugar = document.createTextNode("Sugar content");
+
+    filterForm.append(optName, txtName, br0,
+                      optCarbs, txtCarbs, br1,
+                      optProtein, txtProtein, br2,
+                      optFat, txtFat, br3,
+                      optCalories, txtCalories, br4,
+                      optSugar, txtSugar);
 
     filterFormBox.append(filterForm);
-    
+
+    filterForm.addEventListener("click", (e) => {
+        const selectedValue = document.querySelector("input[name='sortOrder']:checked").value;
+        sortObject(fruits, selectedValue);
+    });
+}
+
+function sortObject(fruits, sortBy)
+{
+    switch (sortBy)
+    {
+        case "byCarbs":
+            break;
+        case "byProtein":
+            break;
+        case "byFat":
+            break;
+        case "byCalories":
+            break;
+        case "bySugar":
+            break;
+        default:
+            searchBox(fruits);
+            break;
+    }
 }
